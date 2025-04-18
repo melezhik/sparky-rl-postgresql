b2c9b7d91bc927724a1556910573369aec1b60ac
Alexey Melezhik <melezhik@gmail.com>
2025-04-18 22:17:41 +0300

    correct check

--- tasks/install/task.bash
+++ tasks/install/task.bash
@@ -1,7 +1,8 @@
+set -e
 sudo dnf update -y
 sudo dnf list postgresql-server
 sudo dnf install -y postgresql-server postgresql
 sudo systemctl enable postgresql
-sudo postgresql-setup --initdb
+sudo postgresql-setup --initdb 2>&1
 sudo systemctl start postgresql
 sudo systemctl status postgresql
